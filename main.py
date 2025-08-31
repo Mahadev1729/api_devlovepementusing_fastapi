d606356422242b23ee5bd1c740f72284a0593a7e
Mahadev1729 <devcampus17@gmail.com>
2025-08-31 15:27:38 +0530

    Refactor FastAPI application structure by moving router inclusion to a separate function

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 ﻿from fastapi import FastAPI
 from core.config import settings
+from apis.api_router import page_router
 
-app=FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
+def include_router(app):
+    app.include_router(page_router)
+    
+def start_application():    
+    app=FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
+    include_router(app)
+    return app
 
-@app.get("/")
-def hello_api():
-    return {"message":"Hello api"}
+app=start_application()
